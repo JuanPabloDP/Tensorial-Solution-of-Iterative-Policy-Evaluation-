88904feb41a4bb95e90d8384a581ccb50e9c434b
JuanPabloDP <juanpablo.dp.glez@gmail.com>
2026-03-07 11:25:17 -0600

    Used a better way to record execution times

--- scripts/tensorialSolution.py
+++ scripts/tensorialSolution.py
@@ -1,23 +1,41 @@
 import numpy as np
 
+#from defineProblem import build_vectors
+
 def iter_policy_evaluation(V_0,R,PI,P,gamma,iters):
-    for i in range(iters):
+    for _ in range(iters):
         V_i=PI@np.tensordot(P,R+gamma*V_0,axes=1)
         V_0=V_i
     return V_i
 
 def policy_improvement(V_0,R,PI,P,gamma,iters):
-    for i in range(iters):
+    for _ in range(iters):
         Q_i=np.tensordot(P,R+gamma*V_0,axes=1)
         V_i=PI@Q_i
         V_0=V_i
     return V_i,Q_i
 
 def iter_policy_evaluation_low(V_0,R,PI,P_low,gamma,iters):
-    for i in range(iters):
+    for _ in range(iters):
         V_i=PI@np.where(P_low == -1, 0, (R+gamma*V_0)[P_low])
         V_0=V_i
     return V_i
+
+def iter_policy_evaluation_conv(V_i,R,PI,P,gamma,iters,delta):
+    for _ in range(iters):
+        V_0=V_i
+        V_i=PI@np.tensordot(P,R+gamma*V_0,axes=1)
+        if np.max(np.abs(V_i-V_0))<delta:
+            break
+    return V_i  
+
+def iter_policy_evaluation_lowconv(V_i,R,PI,P_low,gamma,iters,delta):
+    for _ in range(iters):
+        V_0=V_i
+        V_i=PI@np.where(P_low == -1, 0, (R+gamma*V_0)[P_low])
+        if np.max(np.abs(V_i-V_0))<(delta*(1-gamma)/gamma):
+            break
+    return V_i  
 
 if __name__ == '__main__':
     ...
